9387447b4655a543b0044927c08e186ddb747566
Christophe Pradal <christophe.pradal@inria.fr>
2007-10-16 12:02:33 +0000

    Translate aml file aml_test.py to python.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@3848 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- aml/src/aml/wralea/aml_test.py
+++ aml/src/aml/wralea/aml_test.py
@@ -1,7 +1,8 @@
+from openalea.aml import *
 
 mtg10 = MTG(amapmod_dir + "databases/MTGFiles/AppleTree/wij10.mtg")
 
-gus = VtxList(Scale->3)
+gus = VtxList(Scale=3)
 
 #############################################
 #
@@ -12,16 +13,20 @@ gus = VtxList(Scale->3)
 # 1. sampling function
 # number of internodes per entity
 
-innb(_x) = Size(Components(_x, Scale->4))
+innb = lambda _x: Size(Components(_x, Scale=4))
 
 # 2. Sample
 # set of growth units for a given order 
+#gus_o(_order) = Foreach _x In gus : Select(_x, Order(_x) == _order)
 
-gus_o(_order) = Foreach _x In gus : Select(_x, Order(_x) == _order)
-
+def gus_o(_order):
+    return [Select(_x, Order(_x) == _order) for _x in gus] 
 # Iteration : construction of the sample of values
 
-sample_innb_gu(_order) = Foreach _x In gus_o(_order) : innb(_x)
+#sample_innb_gu(_order) = Foreach _x In gus_o(_order) : innb(_x)
+def sample_innb_gu(_order):
+    return [innb(_x) for _x in gus_o(_order)] 
 
 histo_nb_gu0 = Histogram(sample_innb_gu(0))
 Plot(histo_nb_gu0)
+
