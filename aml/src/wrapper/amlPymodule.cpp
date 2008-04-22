6579623241fa7c5a6aaf3b174676bbfb6def2680
Christophe Pradal <christophe.pradal@inria.fr>
2008-04-22 15:28:19 +0000

    * Add Py_ssize_t type definition for compatibility with Python 2.4
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@4782 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- aml/src/wrapper/amlPymodule.cpp
+++ aml/src/wrapper/amlPymodule.cpp
@@ -47,6 +47,12 @@
 #include "compile.h"
 #include "funcobject.h"
 
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
+
 
 using namespace std;
 
