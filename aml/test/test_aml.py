aec0486f20f2ac94ff400d99f99b97ffbb5561f8
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-06-11 12:32:06 +0000

    add some tests
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@5043 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ aml/test/test_aml.py
@@ -0,0 +1,25 @@
+from openalea.aml import *
+
+
+def test_mtg():
+
+    m = MTG("stand.mtg")
+    assert m
+
+
+def test_agraf():
+    g  = MTG("agraf.mtg")
+
+    dr1 = DressingData("agraf.drf")
+    f1 = PlantFrame(1, Scale=3, DressingData=dr1)
+    #Plot(f1)
+
+    assert g and dr1 and f1
+
+
+
+def test_drf():
+    mtg = MTG("testdrf.mtg")
+    drf = DressingData("testdrf.drf")
+    pf = PlantFrame (0, DressingData=drf)
+    
