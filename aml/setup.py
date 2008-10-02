975dd3a95d875cbfb4aeb0668c7f197139cff143
Christophe Pradal <christophe.pradal@inria.fr>
2008-10-02 06:12:35 +0000

    * Add MAC OS X dynamic library to the package data to install build libraries correctly.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@5565 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- aml/setup.py
+++ aml/setup.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
                        },
       
           # Add package platform libraries if any
-          package_data = {namespace+".aml" : ['*.pyd', '*.so'],},
+          package_data = {namespace+".aml" : ['*.pyd', '*.so', '*.dylib'],},
           include_package_data=True,
           zip_safe = False,
 
