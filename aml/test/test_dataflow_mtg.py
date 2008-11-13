9913b3549fad7e0a774ea3a6c663d66a38ffa8f1
Frederic Boudon <frederic.boudon@cirad.fr>
2008-11-13 09:51:41 +0000

    use alea.run instead of alea.noserun
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@5670 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- aml/test/test_dataflow_mtg.py
+++ aml/test/test_dataflow_mtg.py
@@ -5,12 +5,12 @@ pm.init(verbose=False)
 
 def test_demo_mtg_PlantFrame2():
     """ Test dataflow demo PlantFrame2 """
-    res = noserun(('demo.mtg','PlantFrame2'),{},pm=pm)
+    res = run(('demo.mtg','PlantFrame2'),{},pm=pm)
     assert res == []
 
 def test_demo_mtg_PlantFrame1():
     """ Test dataflow demo PlantFrame1 """
-    res = noserun(('demo.mtg','PlantFrame1'),{},pm=pm)
+    res = run(('demo.mtg','PlantFrame1'),{},pm=pm)
     assert res == []
 
 
