a2cc44594f783579738acffe9c02822fa0793941
Thomas Cokelaer <cokelaer@gmail.com>
2009-01-05 14:54:36 +0000

    add QApplication for warnings
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@5916 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- aml/test/test_dataflow_mtg.py
+++ aml/test/test_dataflow_mtg.py
@@ -1,26 +1,32 @@
 from openalea.core.alea import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 pm = PackageManager()
 pm.init(verbose=False)
 
+app = QApplication([])
+
+
 def test_demo_mtg_PlantFrame2():
     """ Test dataflow demo PlantFrame2 """
-    res = run(('demo.mtg','PlantFrame2'),{},pm=pm)
+    res = run(('demo.mtg', 'PlantFrame2'), {}, pm=pm)
     assert res == []
+
 
 def test_demo_mtg_multiplemtg():
-    """ Test dataflow demo PlantFrame1 """
-    res = run(('demo.mtg','MTGActionOnMultipleMTGs'),{},pm=pm)
+    """ Test dataflow demo multiple MTG"""
+    res = run(('demo.mtg', 'MTGActionOnMultipleMTGs'), {}, pm=pm)
     assert res == []
+
 
 def test_demo_mtg_selection():
-    """ Test dataflow demo PlantFrame1 """
-    res = run(('demo.mtg','MTGComponentSelection'),{},pm=pm)
-
+    """ Test dataflow demo Component Selection """
+    res = run(('demo.mtg', 'MTGComponentSelection'), {}, pm=pm)
     assert res == []
+
 
 def test_demo_mtg_extraction():
-    """ Test dataflow demo PlantFrame1 """
-    res = run(('demo.mtg','MTGExtraction'),{},pm=pm)
+    """ Test dataflow demo MTG extraction """
+    res = run(('demo.mtg', 'MTGExtraction'), {}, pm=pm)
     assert res == []
-
