0d54c5e89d3d0e17aa5fa613d53770f623f7e8ca
Thomas Cokelaer <cokelaer@gmail.com>
2010-02-09 09:06:21 +0000

    Merged release_0.8.0 changes r7524:7837 into the trunk (vplants)
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@7897 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ aml2py/metainfo.py
@@ -0,0 +1,13 @@
+
+[metainfo]
+version = 0.8.0
+release = 0.8
+project = vplants
+name = aml2py
+description = Convert aml files into Python files.
+long_description = aml2py is a package to convert aml files into Python script.
+author = Christophe Pradal
+author_email= christophe.pradal@cirad.fr
+url = http://openalea.gforge.inria.fr
+license = Cecill-C
+
--- aml2py/setup.py
+++ aml2py/setup.py
@@ -1,19 +1,27 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$"
+
 # import
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
-LONG_DESC = """ aml2py convert AML files to python files."""
+from openalea.deploy.metainfo import read_metainfo
 
-version= "0.7.0"
+metadata = read_metainfo('metainfo.ini', verbose=True)
+for key,value in metadata.iteritems():
+    exec("%s = '%s'" % (key, value))
 
-setup(name="aml2py",
+
+
+setup(name=name,
       version=version,
-      description="Convert aml files to python files",
-      author="Christophe Pradal",
-      author_email="christophe.pradal@cirad.fr",
-      url="http://openalea.gforge.inria.fr",
-      license="CECILL-C",
-      long_description=LONG_DESC,
+      description=description,
+      long_description=long_description,
+      author=author,
+      author_email=author_email,
+      url=url,
+      license=license,
+
       py_modules=['aml2py'], 
 
       # entry_points
